69495701632e5f3f218c1a8db4d862ea7d7c2257
Fabian Otto <fabian.otto@familienservice.de>
2012-01-09 12:12:20 +0100

    Using JS to redirect to FBs OAuth Page as documented

--- lib/fb_graph_rails/fb_action_controller_extension.rb
+++ lib/fb_graph_rails/fb_action_controller_extension.rb
@@ -18,7 +18,9 @@ module FbGraphRails::FbActionControllerExtension
           authenticate self.class.fb_model.identify(auth.user)
           return true
         else
-          return redirect_to auth.authorize_uri(self.class.fb_canvas_url).html_safe
+          url = auth.authorize_uri(self.class.fb_canvas_url).html_safe
+          return render :inline => "<script>top.location.href = '#{url}';</script>"
+
         end
       end
     end
